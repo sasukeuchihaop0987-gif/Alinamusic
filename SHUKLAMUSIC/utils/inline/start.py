dfd499fb63e491e189f4331b185bc2c25ddf2454
sasukeuchihaop0987-gif <sasukeuchihaop0987@gmail.com>
2026-09-05 02:12:33 +0530

    Update start.py

--- SHUKLAMUSIC/utils/inline/start.py
+++ SHUKLAMUSIC/utils/inline/start.py
@@ -1,18 +1,5 @@
-# -----------------------------------------------
-# 🔸 StrangerMusic Project
-# 🔹 Developed & Maintained by: Shashank Shukla (https://github.com/itzshukla)
-# 📅 Copyright © 2022 – All Rights Reserved
-#
-# 📖 License:
-# This source code is open for educational and non-commercial use ONLY.
-# You are required to retain this credit in all copies or substantial portions of this file.
-# Commercial use, redistribution, or removal of this notice is strictly prohibited
-# without prior written permission from the author.
-#
-# ❤️ Made with dedication and love by ItzShukla
-# -----------------------------------------------
-
 from pyrogram.types import InlineKeyboardButton
+
 import config
 from SHUKLAMUSIC import app
 
@@ -21,8 +8,11 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["S_B_1"],
+                url=f"https://t.me/{app.username}?startgroup=true",
             ),
+        ],
+        [
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
     ]
@@ -33,18 +23,32 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
+                text="✨ ✚ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴀᴛ ✚",
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_10"], callback_data="shiv_Shashank"),
+            InlineKeyboardButton(
+                text="📢 ═ ᴜᴘᴅᴀᴛᴇs ═", 
+                url="https://t.me/ll_ABOUT_SASUKE_ll"
+            ),
+            InlineKeyboardButton(
+                text="💬 ═ sᴜᴘᴘᴏʀᴛ ═", 
+                url="https://t.me/+W3WrSwmHeaY5NjM9"
+            ),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(
+                text="🎄 ═ ʜᴇʟᴘ ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅ ═", 
+                callback_data="settings_back_helper"
+            )
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+        [
+            InlineKeyboardButton(
+                text="👑 ═ ᴏᴡɴᴇʀ ═", 
+                url="https://t.me/sasuke_qt"
+            )
+        ],
     ]
     return buttons
+ 
