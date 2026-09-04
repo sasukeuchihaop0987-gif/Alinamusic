8441914062e7244bac83f7fefefa22eb142deb6e
sasukeuchihaop0987-gif <sasukeuchihaop0987@gmail.com>
2026-09-04 16:02:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -96,7 +96,7 @@ DEBUG_IGNORE_LOG = True
 
 ###### IMAGE URLS ######
 
-START_IMG_URL = getenv("START_IMG_URL", "https://files.catbox.moe/ak96mx.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://files.catbox.moe/7qj5wo.jpg")
 PING_IMG_URL = getenv("PING_IMG_URL", "https://files.catbox.moe/ak96mx.jpg")
 PLAYLIST_IMG_URL = "https://files.catbox.moe/lrwbj6.jpg"
 STATS_IMG_URL = "https://files.catbox.moe/ak96mx.jpg"
