464b60d65441605f973a885fc8c731485c06905d
sasukeuchihaop0987-gif <sasukeuchihaop0987@gmail.com>
2026-09-05 02:22:29 +0530

    Update start.py

--- SHUKLAMUSIC/utils/inline/start.py
+++ SHUKLAMUSIC/utils/inline/start.py
@@ -23,32 +23,20 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="✨ ✚ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴀᴛ ✚",
+                text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
         [
-            InlineKeyboardButton(
-                text="📢 ═ ᴜᴘᴅᴀᴛᴇs ═", 
-                url="https://t.me/ll_ABOUT_SASUKE_ll"
-            ),
-            InlineKeyboardButton(
-                text="💬 ═ sᴜᴘᴘᴏʀᴛ ═", 
-                url="https://t.me/+W3WrSwmHeaY5NjM9"
-            ),
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
         ],
         [
-            InlineKeyboardButton(
-                text="🎄 ═ ʜᴇʟᴘ ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅ ═", 
-                callback_data="settings_back_helper"
-            )
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
-            InlineKeyboardButton(
-                text="👑 ═ ᴏᴡɴᴇʀ ═", 
-                url="https://t.me/sasuke_qt"
-            )
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_7"], url=config.STATS_SUDO),
         ],
     ]
     return buttons
- 
