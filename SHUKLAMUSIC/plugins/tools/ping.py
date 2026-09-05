01a1a6187dd26173895cf4d91f8de663da1dc03b
sasukeuchihaop0987-gif <sasukeuchihaop0987@gmail.com>
2026-09-05 10:59:14 +0530

    Update ping.py

--- SHUKLAMUSIC/plugins/tools/ping.py
+++ SHUKLAMUSIC/plugins/tools/ping.py
@@ -1,36 +1,22 @@
-# -----------------------------------------------
-# 🔸 StrangerMusic Project
-# 🔹 Developed & Maintained by: Shashank Shukla (https://github.com/itzshukla)
-# 📅 Copyright © 2022 – All Rights Reserved
-#
-# 📖 License:
-# This source code is open for educational and non-commercial use ONLY.
-# You are required to retain this credit in all copies or substantial portions of this file.
-# Commercial use, redistribution, or removal of this notice is strictly prohibited
-# without prior written permission from the author.
-#
-# ❤️ Made with dedication and love by ItzShukla
-# -----------------------------------------------
 from datetime import datetime
+
 from pyrogram import filters
 from pyrogram.types import Message
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
-from config import *
+
+from config import BANNED_USERS, PING_IMG_URL
 from SHUKLAMUSIC import app
 from SHUKLAMUSIC.core.call import SHUKLA
 from SHUKLAMUSIC.utils import bot_sys_stats
 from SHUKLAMUSIC.utils.decorators.language import language
 from SHUKLAMUSIC.utils.inline import supp_markup
-from config import BANNED_USERS, PING_IMG_URL
-import random
 
 
-@app.on_message(filters.command("ping", prefixes=["/"]) & ~BANNED_USERS)
+@app.on_message(filters.command(["ping"], prefixes=["/", "!"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
     start = datetime.now()
     response = await message.reply_photo(
-        random.choice(PING_IMG_URL),
+        photo=PING_IMG_URL,
         caption=_["ping_1"].format(app.mention),
     )
     pytgping = await SHUKLA.ping()
